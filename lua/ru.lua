b9fde4768bb2cdfb8b99e940d50708b7dc0cbdc3
SkulldierGuy <142214319+SkulldierGuy@users.noreply.github.com>
2023-10-13 22:25:26 +0300

    Update ru.lua

--- lua/ru.lua
+++ lua/ru.lua
@@ -4,14 +4,31 @@ return function(mod)
 
 	local Collectible = {
 		[GI("Golden Idol")]={ru={"Золотой Идол","Проклятые богатства"},},
+                [GI("Gun that can kill the Past")]={ru={"Пушка Убивающая Прошлое","Оставь прошлое позади"},},
+                [GI('Birthday Cake')]={ru={"Именинный Торт","Счастливый Я!"},},
+                [GI('Rusty Spoon')]={ru={"Ржавая Ложка","Тебе нравится их трогать..."},},
+                [GI('Newgrounds Tank')]={ru={"Танк Newgrounds","Стань Танком!"},},
+                [GI('Greed Butt')]={ru={"Алчная Жопа","Не трогай!"},},
+                [GI('   Focus   ')]={ru={"Фокус","Забери их ДУШИ"},},
+                [GI('  Focus  ')]={ru={"Фокус",""Забери их ДУШИ"},},
+                [GI(' Focus ')]={ru={"Фокус",""Забери их ДУШИ"},},
+                [GI('Focus')]={ru={"Фокус",""Забери их ДУШИ"},},
+                [GI('The Doorway')]={ru={"Проход","Открой ВСЕ двери"},},
+                [GI('Strange Marble')]={ru={"Странный Марбл","Чудесные цвета..."},},
+                [GI('Is You')]={ru={"Это Ты","ПРЕДМЕТ ЭТО ПРЕПЯТСТВИЕ"},},
+                [GI('Nightmare Tick')]={ru={"Кошмарный Клещ","Эй, убери это!"},},
+                [GI("Spelunker's Pack")]={ru={"Рюкзак Спелеолога","Искатель Сокровищ +12 бомб"},},
+                [GI("Diogenes's Pot")]={ru={"Пифос Диогена","Уничтожь их, начни снова"},},
+                [GI(" Diogenes's Pot ")]={ru={"Пифос Диогена","Уничтожь их, начни снова"},},
+                [GI('George')]={ru={"Джордж","Зри в Корень"},},
 		
 		--[GI('')]={ru={"",""},},	
 	}
 	local Trinket={
-        [GI("Ring of the Snake")]={ru={"Кольцо Змея","Starting hand +2"},},	
+        [GI("Ring of the Snake")]={ru={"Кольцо Змеи","Стартовая рука +2"},},	
 	}
 	local Cards={
-        ['Cow on a Trash Farm']={ru={"Cow on a Trash Farm","Become back your money"},},
+        ['Cow on a Trash Farm']={ru={"Скотина Помойная","Верни свои деньги"},},
         --['']={ru={"",""},},
     }
 
@@ -21,6 +38,6 @@ return function(mod)
 		['Cards'] = Cards,
 		--['Pills'] = Pills,
 	}
-	ItemTranslate.AddModTranslation("war zone", ModTranslate, {ru = true})
+	ItemTranslate.AddModTranslation("warp zone", ModTranslate, {ru = true})
 
 end
