ad795919e89452dfd1c21a5b84bb1e6759cd9426
Goganidze <117683466+Goganidze@users.noreply.github.com>
2023-11-12 03:28:24 +0500

    переделка Вархламова

--- lua/ser_junkan.lua
+++ lua/ser_junkan.lua
@@ -15,7 +15,26 @@ return function (mod)
         end
     end
 
-    ---comment
+    local cacehsagasg = {}
+    local function GetLastFrame(spr)
+        local anm = spr:GetAnimation()
+        if not cacehsagasg[anm] then
+            local frame = spr:GetFrame()
+            local overframe = spr:GetOverlayFrame()
+
+            spr:SetLastFrame()
+            cacehsagasg[anm] = spr:GetFrame()
+
+            spr:SetFrame(frame, true)
+        else
+            return cacehsagasg[anm]
+        end
+    end
+    local lerpAngle = function(a, b, t)
+        return (a - (((a+180)-b)%360-180)*t) 
+    end
+
+
     ---@param ent Entity
     ---@return EntityNPC
     function WarpZone.SpawnNPCProxy(ent)
@@ -90,7 +109,7 @@ return function (mod)
         local entities = Isaac.FindInRadius(bomb.Position, 100)
         local rng = player:GetCollectibleRNG(WarpZone.WarpZoneTypes.COLLECTIBLE_SER_JUNKAN)
         for i, entity in ipairs(entities) do
-            if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE then
+            if entity.Type == EntityType.ENTITY_PICKUP and entity.Variant == PickupVariant.PICKUP_COLLECTIBLE and entity.SubType ~= 0 then
                 Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, -1, entity.Position, entity.Velocity, bomb)
                 entity:Remove()
                 --local loops = rng:RandomInt(3) + 3
@@ -130,69 +149,164 @@ return function (mod)
             fam.GridCollisionClass = EntityGridCollisionClass.GRIDCOLL_GROUND
         end
         fam.EntityCollisionClass = EntityCollisionClass.ENTCOLL_ALL
-        local entities = Isaac.FindInRadius(fam.Position, 100, EntityPartition.ENEMY)
-        for i, entity in ipairs(entities) do
-            if entity:IsVulnerableEnemy() then
-                if enemyEntity == nil then
-                    enemyEntity = entity
-                else
-                    if fam.Position:Distance(enemyEntity.Position) > fam.Position:Distance(entity.Position) then
+
+        if fam.State == 0 then
+            
+            --[[local entities = Isaac.FindInRadius(fam.Position, 100, EntityPartition.ENEMY)
+            for i, entity in ipairs(entities) do
+                if entity:IsVulnerableEnemy() then
+                    if enemyEntity == nil then
                         enemyEntity = entity
+                    else
+                        if fam.Position:Distance(enemyEntity.Position) > fam.Position:Distance(entity.Position) then
+                            enemyEntity = entity
+                        end
+                    end
+                end
+            end]]
+            fam:PickEnemyTarget(200, 1, 2)
+            enemyEntity = fam.Target
+            local followPlayer
+            if enemyEntity ~= nil and (enemyEntity.Position-fam.Position):Length() > math.min(5, enemyEntity.Size) then
+                --followPos = normalizedirection(fam.Position, enemyEntity.Position, true)
+                followPos = enemyEntity.Position
+                animName = "Walk"
+            elseif player.Position:Distance(fam.Position) > 0 and enemyEntity == nil then
+                --followPos = normalizedirection(fam.Position, player.Position, false)
+                followPos = player.Position -- (player.Position-fam.Position):Resized(60)
+                animName = fam.Velocity:Length()>0.3 and "Walk" or animName
+                followPlayer = true
+            end
+            --if enemyEntity ~= nil and (enemyEntity.Position-fam.Position):Length() <= 15 then
+            --    animName = "Attack"
+            --end
+            if followPos then
+                --fam.Velocity = (followPos-fam.Position):Resized(followPos:Distance(fam.Position)/40)    --followPos
+                local dist = followPos:Distance(fam.Position)
+                if dist < 60 or game:GetRoom():CheckLine(followPos, fam.Position, 0) then
+                    local power --= followPos:Distance(fam.Position)/90 + 6
+                    if followPlayer then
+                        power = math.min(10, math.max(0, followPos:Distance(fam.Position)-60)/10 )
+                    else
+                        power = followPos:Distance(fam.Position)/90 + 6
+                    end
+                    --power = math.min(dist/2, power)
+                    fam.Velocity = fam.Velocity * 0.6 + (followPos-fam.Position):Resized(power) * 0.4
+                else
+                    WarpZone.GetPathFinder(fam):FindGridPath(followPos, 1.1, 0, true) --0.3 + dist/220
+                end
+            end
+            --fam:FollowPosition(followPos)
+            
+            animName = animName .. tostring(junkCount)
+            if spr:GetAnimation() ~= animName then
+                if spr:GetAnimation():sub(-1) ~= animName:sub(-1) then
+                    Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, -1, fam.Position, Vector(0, 0), fam)
+                end
+                spr:Play(animName)
+            end
+            if fam.Velocity.X < 0 then
+                spr.FlipX = true
+            else
+                spr.FlipX = false
+            end
+
+            if enemyEntity and enemyEntity.Position:Distance(fam.Position) <= 15+enemyEntity.Size then
+                fam.State = 1
+                fam.Target = enemyEntity
+
+                spr:Play("Attack" .. tostring(junkCount), true)
+                if junkCount < 4 then
+                    spr:SetFrame(6)
+                elseif junkCount > 5 then
+                    fam.State = 2
+                end
+            end
+        
+        elseif fam.State == 1 then
+            local damage = 0
+            local mas = false
+            if  spr:IsEventTriggered("BumpAttack") then
+                damage = junkCount/2 + 1
+            elseif  spr:IsEventTriggered("SwordSwing") then
+                damage = junkCount
+                mas = 40
+            elseif  spr:IsEventTriggered("SpinAttack") then
+                damage = 0.7
+                if junkCount == 7 then
+                    damage = 1
+                end
+                mas = 70
+            end
+            if fam.Target and damage > 0 then
+                if fam.Player:HasCollectible(CollectibleType.COLLECTIBLE_BFFS) then
+                    damage = damage * 2
+                end
+                --if not mas then
+                    fam.Target:TakeDamage(damage, 0, EntityRef(fam), 1)
+                if  mas then
+                    local list = Isaac.FindInRadius(fam.Position, mas, EntityPartition.ENEMY)
+                    for i=1, #list do
+                        local ent = list[i]
+                        if ent:IsVulnerableEnemy() and ent:IsActiveEnemy() then
+                            ent:TakeDamage(damage/2, 0, EntityRef(fam), 1)
+                        end
                     end
                 end
             end
-        end
-        if enemyEntity ~= nil and (enemyEntity.Position-fam.Position):Length() > math.min(5, enemyEntity.Size) then
-            --followPos = normalizedirection(fam.Position, enemyEntity.Position, true)
-            followPos = enemyEntity.Position
-            animName = "Walk"
-        elseif player.Position:Distance(fam.Position) > 60 and enemyEntity == nil then
-            --followPos = normalizedirection(fam.Position, player.Position, false)
-            followPos = player.Position
-            animName = "Walk"
-        end
-        if enemyEntity ~= nil and (enemyEntity.Position-fam.Position):Length() <= 15 then
-            animName = "Attack"
-        end
-        if followPos then
-            --fam.Velocity = (followPos-fam.Position):Resized(followPos:Distance(fam.Position)/40)    --followPos
-            local dist = followPos:Distance(fam.Position)
-            if dist < 60 or game:GetRoom():CheckLine(followPos, fam.Position, 0) then
-                local power = followPos:Distance(fam.Position)/90 + 6
-                power = math.min(dist/2, power)
-                fam.Velocity = (followPos-fam.Position):Resized(power)
-            else
-                WarpZone.GetPathFinder(fam):FindGridPath(followPos, 1.1, 0, true) --0.3 + dist/220
+
+            if GetLastFrame(spr) == spr:GetFrame() then
+                fam.State = 0
             end
-        end
-        --fam:FollowPosition(followPos)
-        
-        animName = animName .. tostring(junkCount)
-        if spr:GetAnimation() ~= animName then
-            if spr:GetAnimation():sub(-1) ~= animName:sub(-1) then
-                Isaac.Spawn(EntityType.ENTITY_EFFECT, EffectVariant.POOF01, -1, fam.Position, Vector(0, 0), fam)
+            if fam.Target then
+                local power = fam.Target.Position:Distance(fam.Position)-fam.Target.Size
+                fam.Velocity = fam.Velocity * 0.8 + (fam.Target.Position-fam.Position):Resized(power/2) * 0.2
             end
-            spr:Play(animName)
-        end
-        if fam.Velocity.X < 0 then
-            spr.FlipX = true
-        else
-            spr.FlipX = false
-        end
-    
-        local damage = 0
-        if enemyEntity and spr:IsEventTriggered("BumpAttack") then
-            damage = junkCount/2 + 1
-        elseif enemyEntity and spr:IsEventTriggered("SwordSwing") then
-            damage = junkCount
-        elseif enemyEntity and spr:IsEventTriggered("SpinAttack") then
-            damage = 0.7
+            fam.Velocity = fam.Velocity * (1-spr:GetFrame()*0.02)
+
+        elseif fam.State == 2 then
+            local damage = 0.7
+            local mas = 40
             if junkCount == 7 then
                 damage = 1
             end
-        end
-        if enemyEntity and damage > 0 then
-            enemyEntity:TakeDamage(damage, 0, EntityRef(fam), 1)
+            if fam.Player:HasCollectible(CollectibleType.COLLECTIBLE_BFFS) then
+                damage = damage * 2
+                mas = mas + 20
+            end
+
+            if not fam.Target then
+                fam:PickEnemyTarget(200, 1, 2)
+            end
+
+            if spr:IsEventTriggered("SpinAttack") then
+                local list = Isaac.FindInRadius(fam.Position, mas, EntityPartition.ENEMY)
+                for i=1, #list do
+                    local ent = list[i]
+                    if ent:IsVulnerableEnemy() and ent:IsActiveEnemy() then
+                        ent:TakeDamage(damage, 0, EntityRef(fam), 1)
+                    end
+                end
+            end
+            if not spr:WasEventTriggered("SpinAttack") then
+                fam.Velocity = fam.Velocity * 0.82 + (fam.Target.Position-fam.Position):Resized(5) * 0.1
+            elseif fam.Target and fam.Hearts == 0 then
+                fam.Hearts = 1
+                --local power = fam.Target.Position:Distance(fam.Position)-fam.Target.Size
+                --fam.Velocity = fam.Velocity * 0.5 + (fam.Target.Position-fam.Position):Resized(10) * 0.5
+                if junkCount == 7 then
+                    fam.Velocity = (fam.Target.Position-fam.Position):Resized(10)
+                else
+                    fam.Velocity = (fam.Target.Position-fam.Position):Resized(7)
+                end
+            elseif fam.Target and fam.Hearts == 1 then
+                local ang = (fam.Target.Position-fam.Position):GetAngleDegrees()
+                fam.Velocity = Vector(fam.Velocity:Length(), 0):Rotated(lerpAngle(fam.Velocity:GetAngleDegrees(), ang, 0.1))
+            end
+            if GetLastFrame(spr) == spr:GetFrame() then
+                fam.State = 0
+                fam.Hearts = 0
+            end
         end
     end
     WarpZone:AddCallback(ModCallbacks.MC_FAMILIAR_UPDATE, WarpZone.update_junkan, SerJunkanWalk)
