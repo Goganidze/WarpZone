eb2d3edfe3030634270312667d48e44e94ddce7b
Goganidze <117683466+Goganidze@users.noreply.github.com>
2024-11-15 01:11:26 +0500

    circle laser synergy

--- main.lua
+++ main.lua
@@ -4143,7 +4143,8 @@ function WarpZone:updateLaser(laser)
         if data.WZ_ShotArrow == true then
             local params = player:GetTearHitParams(WeaponType.WEAPON_TEARS, 1.5)
             
-            WarpZone.ShotArrow(laser.Position, Vector.FromAngle(laser.Angle):Resized(player.ShotSpeed*12), 
+            local anlge = laser:IsCircleLaser() and laser.Velocity:GetAngleDegrees()-180 or laser.Angle
+            WarpZone.ShotArrow(laser.Position, Vector.FromAngle(anlge):Resized(player.ShotSpeed*12), 
                 player, params.TearScale, player.Damage*2, nil, true)
 
             data.WZ_ShotArrow = 30
@@ -4153,13 +4154,17 @@ function WarpZone:updateLaser(laser)
                 if  laser.FrameCount % 3 == 0 then --laser.FrameCount < 30 and
                     local params = player:GetTearHitParams(WeaponType.WEAPON_TEARS, .7)
                     local miss = laser:GetDropRNG():RandomInt(30)-15
-                    WarpZone.ShotArrow(laser.Position, Vector.FromAngle(laser.Angle+miss):Resized(player.ShotSpeed*12), 
+					
+                    local anlge = laser:IsCircleLaser() and (laser.Velocity:GetAngleDegrees()-180) or laser.Angle
+                    WarpZone.ShotArrow(laser.Position, Vector.FromAngle(anlge+miss):Resized(player.ShotSpeed*12), 
                         player, params.TearScale, player.Damage*.5, true)
                 end
                 if data.WZ_ShotArrow <= 0 then   --laser.FrameCount % 30 == 0 then
                     local miss = laser:GetDropRNG():RandomInt(30)-15
                     local params = player:GetTearHitParams(WeaponType.WEAPON_TEARS, 1)
-                    WarpZone.ShotArrow(laser.Position, Vector.FromAngle(laser.Angle+miss):Resized(player.ShotSpeed*12), 
+                    
+                    local anlge = laser:IsCircleLaser() and laser.Velocity:GetAngleDegrees()-180 or laser.Angle
+                    WarpZone.ShotArrow(laser.Position, Vector.FromAngle(anlge+miss):Resized(player.ShotSpeed*12), 
                         player, params.TearScale, player.Damage*1, nil, true)
                 end
             end
